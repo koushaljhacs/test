816395a6431d1799d0653946b7fa4e03081bd4fe
koushaljhacs <koushaljha.cs@gmail.com>
2026-02-05 14:08:56 +0530

    commit

--- test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        // write your code
-        System.out.println("hello");
-        System.out.println("hell0 abhay here");
-    }
-}
